c6a86407bb445ee2ea46bafeac740049a55860d3
m-demidoff <morkva0morkva@gmail.com>
2021-10-26 16:43:53 +0300

    Реализованы функции multiplicative_inverse() и generate_keypair()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -15,7 +15,51 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for c in plaintext:
+
+        # проверить, является ли символ заглавной буквой
+        if c.isupper():
+
+            c_index = ord(c) - ord("A")
+
+            # выполнить отрицательный сдвиг
+            new_index = (c_index + shift) % 26
+
+            # преобразовать в новый символ
+            new_unicode = new_index + ord("A")
+
+            new_character = chr(new_unicode)
+
+            # добавление к простой строке
+            ciphertext += new_character
+
+        elif c.islower():
+            # найти положение в 0-25
+            c_unicode = ord(c)
+
+            c_index = ord(c) - ord("a")
+
+            # выполнить отрицательный сдвиг
+            new_index = (c_index + shift) % 26
+
+            # преобразовать в новый символ
+            new_unicode = new_index + ord("a")
+
+            new_character = chr(new_unicode)
+
+            # добавление к простой строке
+            ciphertext += new_character
+        elif c.isdigit():
+
+            # если это число, сдвинуть его фактическое значение
+            c_new = int(c)
+
+            ciphertext += str(c_new)
+
+        else:
+
+            # если нет ни алфавита, ни числа, оставьте все как есть
+            ciphertext += c
     return ciphertext
 
 
@@ -33,7 +77,38 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for c in ciphertext:
+        if c.isupper():
+
+            c_index = ord(c) - ord("A")
+
+            new_index = (c_index - shift) % 26
+
+            new_unicode = new_index + ord("A")
+
+            new_character = chr(new_unicode)
+
+            plaintext += new_character
+
+        elif c.islower():
+
+            c_unicode = ord(c)
+
+            c_index = ord(c) - ord("a")
+
+            new_index = (c_index - shift) % 26
+
+            new_unicode = new_index + ord("a")
+
+            new_character = chr(new_unicode)
+
+            plaintext += new_character
+        elif c.isdigit():
+            c_new = int(c)
+
+            plaintext += str(c_new)
+        else:
+            plaintext += c
     return plaintext
 
 
