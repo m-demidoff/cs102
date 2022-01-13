828d07c1624b8cb61212a929e47e2e8f8ae805c2
m-demidoff <90193439+m-demidoff@users.noreply.github.com>
2022-01-14 02:14:11 +0300

    Add files via upload
    fixed caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -15,51 +15,25 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    for c in plaintext:
-
-        # проверить, является ли символ заглавной буквой
-        if c.isupper():
-
-            c_index = ord(c) - ord("A")
-
-            # выполнить отрицательный сдвиг
-            new_index = (c_index + shift) % 26
-
-            # преобразовать в новый символ
-            new_unicode = new_index + ord("A")
-
+    for character in plaintext:
+        if character.isupper():  # проверить, является ли символ заглавной буквой
+            c_index = ord(character) - ord("A")
+            new_index = (c_index + shift) % 26  # выполнить отрицательный сдвиг
+            new_unicode = new_index + ord("A")  # преобразовать в новый символ
             new_character = chr(new_unicode)
-
-            # добавление к простой строке
-            ciphertext += new_character
-
-        elif c.islower():
-            # найти положение в 0-25
-            c_unicode = ord(c)
-
-            c_index = ord(c) - ord("a")
-
-            # выполнить отрицательный сдвиг
-            new_index = (c_index + shift) % 26
-
-            # преобразовать в новый символ
-            new_unicode = new_index + ord("a")
-
+            ciphertext += new_character  # добавление к простой строке
+        elif character.islower():
+            c_unicode = ord(character)  # найти положение в 0-25
+            c_index = ord(character) - ord("a")
+            new_index = (c_index + shift) % 26  # выполнить отрицательный сдвиг
+            new_unicode = new_index + ord("a")  # преобразовать в новый символ
             new_character = chr(new_unicode)
-
-            # добавление к простой строке
-            ciphertext += new_character
-        elif c.isdigit():
-
-            # если это число, сдвинуть его фактическое значение
-            c_new = int(c)
-
+            ciphertext += new_character  # добавление к простой строке
+        elif character.isdigit():
+            c_new = int(character)  # если это число, сдвинуть его фактическое значение
             ciphertext += str(c_new)
-
         else:
-
-            # если нет ни алфавита, ни числа, оставьте все как есть
-            ciphertext += c
+            ciphertext += character  # если нет ни алфавита, ни числа, оставить все как есть
     return ciphertext
 
 
@@ -77,38 +51,25 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    for c in ciphertext:
-        if c.isupper():
-
-            c_index = ord(c) - ord("A")
-
+    for character in ciphertext:
+        if character.isupper():
+            c_index = ord(character) - ord("A")
             new_index = (c_index - shift) % 26
-
             new_unicode = new_index + ord("A")
-
             new_character = chr(new_unicode)
-
             plaintext += new_character
-
-        elif c.islower():
-
-            c_unicode = ord(c)
-
-            c_index = ord(c) - ord("a")
-
+        elif character.islower():
+            c_unicode = ord(character)
+            c_index = ord(character) - ord("a")
             new_index = (c_index - shift) % 26
-
             new_unicode = new_index + ord("a")
-
             new_character = chr(new_unicode)
-
             plaintext += new_character
-        elif c.isdigit():
-            c_new = int(c)
-
+        elif character.isdigit():
+            c_new = int(character)
             plaintext += str(c_new)
         else:
-            plaintext += c
+            plaintext += character
     return plaintext
 
 
