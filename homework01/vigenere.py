650a01e078792fd023ac62e65a3667ece02cea33
m-demidoff <90193439+m-demidoff@users.noreply.github.com>
2022-01-14 03:39:06 +0300

    Add files via upload

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,7 +1,6 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
-
     >>> encrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> encrypt_vigenere("python", "a")
@@ -9,8 +8,25 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
+    kw = []
+    a = 0
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for i in keyword:
+        if 65 <= ord(i) <= 90:
+            kw.insert(a, ord(i) - 65)
+        else:
+            kw.insert(a, ord(i) - 97)
+        a += 1
+    kwlen = a
+    a = 0
+    for j in plaintext:
+        if a >= kwlen:
+            a = 0
+        if 65 <= ord(j) <= 90:
+            ciphertext += chr((ord(j) - 65 + kw[a]) % 26 + 65)
+        else:
+            ciphertext += chr((ord(j) - 97 + kw[a]) % 26 + 97)
+        a += 1
     return ciphertext
 
 
@@ -25,6 +41,29 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
+    kw = []
+    a = 0
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for i in keyword:
+        if 65 <= ord(i) <= 90:
+            kw.insert(a, ord(i) - 65)
+        else:
+            kw.insert(a, ord(i) - 97)
+        a += 1
+    kwlen = a
+    a = 0
+    for j in ciphertext:
+        if a >= kwlen:
+            a = 0
+        if 65 <= ord(j) <= 90:
+            if ord(j) - 65 - kw[a] < 0:
+                plaintext += chr((ord(j) - 65 - kw[a]) + 91)
+            else:
+                plaintext += chr(ord(j) - kw[a])
+        else:
+            if ord(j) - 97 - kw[a] < 0:
+                plaintext += chr((ord(j) - 97 - kw[a]) + 123)
+            else:
+                plaintext += chr(ord(j) - kw[a])
+        a += 1
     return plaintext
