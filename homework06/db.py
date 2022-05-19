20c7347ccc956126854816f18de5022fe24e2c12
m-demidoff <90193439+m-demidoff@users.noreply.github.com>
2022-05-19 17:13:40 +0300

    Homework06

--- homework06/db.py
+++ homework06/db.py
@@ -1,15 +1,14 @@
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
+from scraputils import get_news  # type: ignore
+from sqlalchemy import Column, Integer, String, create_engine  # type: ignore
+from sqlalchemy.ext.declarative import declarative_base  # type: ignore
+from sqlalchemy.orm import sessionmaker  # type: ignore
 
 Base = declarative_base()
 engine = create_engine("sqlite:///news.db")
 session = sessionmaker(bind=engine)
 
 
-class News(Base):
+class News(Base):  # type: ignore
     __tablename__ = "news"
     id = Column(Integer, primary_key=True)
     title = Column(String)
@@ -19,4 +18,19 @@ class News(Base):
     points = Column(Integer)
     label = Column(String)
 
+
 Base.metadata.create_all(bind=engine)
+
+if __name__ == "__main__":
+    s = session()
+    news_list = get_news("https://news.ycombinator.com/newest", n_pages=1)
+    for i in enumerate(news_list):
+        news = News(
+            title=news_list[i]["title"],
+            author=news_list[i]["author"],
+            url=news_list[i]["url"],
+            comments=news_list[i]["comments"],
+            points=news_list[i]["points"],
+        )
+        s.add(news)
+        s.commit()
--- homework06/hackernews.py
+++ homework06/hackernews.py
@@ -1,36 +1,89 @@
-from bottle import (
-    route, run, template, request, redirect
-)
+import string
 
-from scrapper import get_news
-from db import News, session
 from bayes import NaiveBayesClassifier
+from bottle import redirect, request, route, run, template  # type: ignore
+from db import News, session
+from scraputils import get_news
 
-
+@route("/")
 @route("/news")
 def news_list():
     s = session()
     rows = s.query(News).filter(News.label == None).all()
-    return template('news_template', rows=rows)
+    return template("news_template", rows=rows)
 
 
 @route("/add_label/")
 def add_label():
-    # PUT YOUR CODE HERE
+    query = request.query.decode()
+    id = int(query["id"])
+    label = query["label"]
+    s = session()
+    s.query(News).filter(News.id == id).update({News.label: label})
+    s.commit()
     redirect("/news")
 
 
 @route("/update")
 def update_news():
-    # PUT YOUR CODE HERE
+    news = get_news("https://news.ycombinator.com/newest", 3)
+    s = session()
+    for new in news:
+        if (
+            len(s.query(News.author).filter_by(author="author").all()) == 0
+            or len(s.query(News.title).filter_by(title="title").all()) == 0
+        ):
+            s.add(
+                News(
+                    title=new["title"],
+                    author=new["author"],
+                    url=new["url"],
+                    points=new["points"],
+                    comments=new["comments"],
+                )
+            )
+    s.commit()
     redirect("/news")
+
+
+def clean(s):
+    translator = str.maketrans("", "", string.punctuation)
+    return s.translate(translator)
 
 
 @route("/classify")
 def classify_news():
-    # PUT YOUR CODE HERE
+    s = session()
+    classifier = NaiveBayesClassifier()
+    X_train = [r.title for r in s.query(News).filter(News.label != None).all()][0:300]
+    y_train = [r.label for r in s.query(News).filter(News.label != None).all()][0:300]
+    predict_db = s.query(News).filter(News.label == None).all()
+    X_predict = [r.title for r in predict_db][0:100]
+    classifier.fit(X_train, y_train)
+    labels = classifier.predict(X_predict)
+    for i in range(100):
+        predict_db[i].label = labels[i]
+    classified = [x for x in predict_db if x.label == "good"]
+    classified.extend([x for x in predict_db if x.label == "maybe"])
+    classified.extend([x for x in predict_db if x.label == "never"])
+    return template("recs", rows=classified)
+
+@route("/recommendations")
+def recommendations():
+    global classifier
+    if classifier:
+        news_list = get_news("https://news.ycombinator.com/newest", 1)
+        titles = [n["title"] for n in news_list]
+        normalized_titles = []
+        for title in titles:
+            normalized_titles.append((title))
+        labels = classifier.predict(normalized_titles)
+        for i in range(len(news_list)):
+            news_list[i]["label"] = labels[i]
+        return template("news_recommendations", rows=news_list)
+    else:
+        return redirect("/classify")
 
 
 if __name__ == "__main__":
-    run(host="localhost", port=8080)
-
+    run(host="localhost", port=8011)
